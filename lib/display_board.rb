9d84e8a9c2aa011d9418610dd5a3ed9be217dc5c
taryn yaeger <tsyaeger@gmail.com>
2018-05-06 00:38:21 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,12 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
-board = [" "] * 9
-ln = "-----------"
+
 
 def display_board
-puts board[0] + board[1] + board[2]
-puts board[3] + board[4] + board[5]
-puts board[6] + board[7] + board[8]
+  board = [" "] * 9
+  ln = "-----------"
+  puts board[0] + board[1] + board[2]
+  puts board[3] + board[4] + board[5]
+  puts board[6] + board[7] + board[8]
 
 display_board
