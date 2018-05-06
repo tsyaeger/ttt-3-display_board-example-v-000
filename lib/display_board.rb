b150813ed03d08bec97ac49c519ff12f59392d02
taryn yaeger <tsyaeger@gmail.com>
2018-05-06 00:40:25 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,6 +8,7 @@ def display_board
   puts board[0] + "|" board[1] + "|" + board[2]
   puts ln
   puts board[3] + "|" + board[4] + "|" + board[5]
+  puts ln
   puts board[6] + "|" + board[7] + "|" + board[8]
 
 display_board
