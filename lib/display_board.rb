4678b665db7e9ff94c01b8073d3171a6dce51076
taryn yaeger <tsyaeger@gmail.com>
2018-05-06 00:40:17 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,8 +5,9 @@
 def display_board
   board = [" "] * 9
   ln = "-----------"
-  puts board[0] + board[1] + board[2]
-  puts board[3] + board[4] + board[5]
-  puts board[6] + board[7] + board[8]
+  puts board[0] + "|" board[1] + "|" + board[2]
+  puts ln
+  puts board[3] + "|" + board[4] + "|" + board[5]
+  puts board[6] + "|" + board[7] + "|" + board[8]
 
 display_board
